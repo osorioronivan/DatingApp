677c8acf3a60fde54c1581e23ed3448c5c37df7f
osorioronivan <osorioronivan05@gmail.com>
2024-11-13 20:25:54 +0800

    consuming the api on Angular project

--- API/Program.cs
+++ API/Program.cs
@@ -1,27 +1,41 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using API.Data; // Add this line
+using API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+
+// Define a named CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlite(connectionString); // Adjust this connection string as needed
+    options.UseSqlite(connectionString);
 });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Enable CORS in development
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("AllowAngularApp");
     app.UseSwagger();
     app.UseSwaggerUI();
-};
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c83ff39612d2957c8da9aff647b4ef44da4399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5a8b2fe9bba537d92d5204d522e6c5fab7571e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
